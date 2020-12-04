09ba898c34245f81fad36b7df692b890eac1c137
Kenneth Le-Tran <kenneth.letran@gmail.com>
2020-12-04 13:26:38 -0600

    accepting incoming merge from master to main

--- app.js
+++ app.js
@@ -1,41 +1,3 @@
-// Scroll indicator appears after a few seconds
-<<<<<<< HEAD
-const main = document.querySelector('.main');
-function addScrollIndicator() {
-    const scrollIndicator = main.lastElementChild;
-    scrollIndicator.classList.add('scroll-indicator')
-}
-setTimeout(addScrollIndicator, 2000);
-
-
-// Have card fade in when scrolled to page
-const cards = document.querySelectorAll('.card');
-function cardFadeObserver(cards) {
-    cards.forEach(card => {
-        if(!card.isIntersecting) {
-            return;
-        } else {
-            card.target.classList.add('fade-in');
-            appearOnScroll.unobserve(card.target);
-        }
-    });
-}
-const appearOnScroll = new IntersectionObserver(cardFadeObserver, { threshold: 1 });
-cards.forEach(card => {
-    appearOnScroll.observe(card);
-})
-
-
-// Type in name
-=======
-const biography = document.querySelector('.biography');
-function addScrollIndicator() {
-    const scrollIndicator = biography.lastElementChild;
-    scrollIndicator.classList.add('scroll-indicator')
-}
-setTimeout(addScrollIndicator, 5000);
-
-
 // Fade in elements on scroll
 const cards = document.querySelectorAll('.card');
 const thumbnails = document.querySelectorAll('.thumbnail-pic');
@@ -58,4 +20,3 @@ cards.forEach(card => {
 thumbnails.forEach(thumbnail => {
     appearOnScroll.observe(thumbnail);
 })
->>>>>>> master
